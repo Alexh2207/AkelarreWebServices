8ce93ba6119267e38664919254c9b738bf046557
Alexh23 <a.hontanillab@alumnos.upm>
2024-10-27 20:06:11 +0100

    Cleaning project. Starting from sratch

--- Bookish - Tu suscripción literaria_files/logconsent.js
+++
@@ -1,3 +0,0 @@
-CookieConsent.setCookie("{stamp:%27yXcQd7s9ymrAf4b6yWYUF619oYS2VTbMAMt70jAcHP+0uXGoXS4vpw==%27%2Cnecessary:true%2Cpreferences:false%2Cstatistics:false%2Cmarketing:false%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1711977293566%2Cregion:%27es%27}", new Date(1743513293566), '/', '', '');
-if (typeof CookieConsent.removeBulkReset!='undefined') {CookieConsent.removeBulkReset();}
-CookieConsent.userCountry = 'es';
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is the code for the Akelare Book Club Web page.
 
-The aim of this is to develop a static web page hosted in the Azure Cloud
+Lista de requisitos:
+
+
